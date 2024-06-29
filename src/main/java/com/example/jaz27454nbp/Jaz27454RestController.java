6a857373bacf3437d6e9a385b0f43a5dffe45e9f
aleksander <abastek@kanga.exchange>
2024-06-29 19:30:17 +0200

    add docs to controller

--- src/main/java/com/example/jaz27454nbp/Jaz27454RestController.java
+++ src/main/java/com/example/jaz27454nbp/Jaz27454RestController.java
@@ -1,11 +1,12 @@
 package com.example.jaz27454nbp;
 
-import jakarta.annotation.Nullable;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.Instant;
-import java.time.ZonedDateTime;
 
 @RestController
 @RequestMapping
@@ -16,6 +17,12 @@ public class Jaz27454RestController {
         this.jaz27454NbpService = jaz27454NbpService;
     }
 
+    @Operation(summary = "Get average currency rate from range of start and end date.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode="200", description = "Currency average rate retrieved Successfully"),
+            @ApiResponse(responseCode = "400", description = "Wrong data was passed or days amount limit exceeded.", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Currency not found.", content = @Content)
+    })
     @GetMapping("/currency/{code}")
     public ResponseEntity<Currency> getCurrency(@PathVariable("code") String code, @RequestBody DateRequest dateRequest) {
         return ResponseEntity.ok(jaz27454NbpService.getCurrency(code, dateRequest.getStartDate(), dateRequest.getEndDate()));
